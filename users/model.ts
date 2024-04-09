0abf0240077158edf2c18567993931234b9ad26b
Rishuke <balasubramaniam.rishi@gmail.com>
2024-04-09 20:20:22 +0200

    Update: Add Ban at model.ts

--- users/model.ts
+++ users/model.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 const utilisateursSchema = new mongoose.Schema({
-  email: {type: String, required: true,unique: true},
-  password: {type: String, required: true},
-  salt : {type: String, required: true},
-  role : {type: String, required: true},
-  createdAt : {type: Date, default: Date.now},
-},{strict: false});
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  salt: { type: String, required: true },
+  role: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  ban: { type: Boolean, default: false } 
+}, { strict: false });
+
 
 export const User = mongoose.model("users", utilisateursSchema);
