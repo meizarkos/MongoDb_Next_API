4258a10a14db39b72a49d0e9bebab3c310c6e5be
Rishuke <balasubramaniam.rishi@gmail.com>
2024-04-09 23:00:56 +0200

    Update json and true

--- users/route.ts
+++ users/route.ts
@@ -25,10 +25,10 @@ const isAdmin = async (req: any, res: Response, next: Function) => {
     if (user && user.role === "admin") {
       next();
     } else {
-      return res.status(403).send({ message: "Accès refusé" });
+      return res.status(403).json({ message: "Accès refusé" });
     }
   } catch (error) {
-    return res.status(500).send({ message: "Erreur interne du serveur" });
+    return res.status(500).json({ message: "Erreur interne du serveur" });
   }
 };
 
@@ -37,23 +37,23 @@ routerUsers.patch("/banArtiste/:id", isAdmin, async (req, res) => {
     const artist = await User.findById(req.params.id) as Artiste;
 
     if (artist) {
-      artist.ban = false;
+      artist.ban = true;
       await User.findByIdAndUpdate(req.params.id, artist);
       res.send({ message: "Artiste banni avec succès" });
     } else {
-      res.status(404).send({ message: "Artiste non trouvé" });
+      res.status(404).json({ message: "Artiste non trouvé" });
     }
   } catch (error) {
-    res.status(500).send({ message: "Erreur lors du bannissement de l'artiste" });
+    res.status(500).json({ message: "Erreur lors du bannissement de l'artiste" });
   }
 });
 
 routerUsers.delete("/user/:id", isAdmin, async (req, res) => {
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.send({ message: "Utilisateur supprimé avec succès" });
+    res.json({ message: "Utilisateur supprimé avec succès" });
   } catch (error) {
-    res.status(500).send({ message: "Erreur lors de la suppression de l'utilisateur" });
+    res.status(500).json({ message: "Erreur lors de la suppression de l'utilisateur" });
   }
 });
 
@@ -63,12 +63,12 @@ routerUsers.post("/login", async (req, res) => {
     const user = await User.findOne({ email });
 
     if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).send({ message: "Email ou mot de passe incorrect" });
+      return res.status(401).json({ message: "Email ou mot de passe incorrect" });
     }
 
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!, { expiresIn: '2h' });
-    res.send({ token });
+    res.json({ token });
   } catch (error) {
-    res.status(500).send({ message: "Erreur lors de la connexion" });
+    res.status(500).json({ message: "Erreur lors de la connexion" });
   }
 });
